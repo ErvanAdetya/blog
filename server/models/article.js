04152be1a39da745bf37682cc59d1b64a731ba7b
Ervan Adetya <vaanz.adetya@gmail.com>
2018-03-19 12:45:11 +0700

    Initial Server

---
+++ server/models/article.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+module.exports = mongoose.model('Article', schema({
+})
+)
---
+++ server/routes/users.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const router = require('mongoose').Router();
+
+module.exports = {
+    
+}
